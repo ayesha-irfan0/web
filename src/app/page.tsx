33df316c998301afb170adfa86aa0d99035fd640
ayesha irfan <wwwayesha0irfan@gmail.com>
2025-09-03 22:23:59 +0500

    make changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <div className='text-4xl text-center'><figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+      import Image from "modern-ecommerce-interface.png"
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/modern-ecommerce-interface.png" alt="" width="384" height="512"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
